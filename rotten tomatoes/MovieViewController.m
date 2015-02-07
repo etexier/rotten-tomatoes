f728ab427f2e7db4257763e004c2911211b24987
Emmanuel Texier <emmanuel.texier@magnet.com>
2015-02-07 13:37:32 -0800

    Fix issue with wifi-off

--- rotten tomatoes/MovieViewController.m
+++ rotten tomatoes/MovieViewController.m
@@ -56,27 +56,6 @@
     
     [self loadData];
     
-    
-    // this is the old code before using Rest2Mobile generation
-    /* 
-     
-    NSString *url = getMoviesString;
-    
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
-                             
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:[NSOperationQueue mainQueue]
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-                               NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-     
-                               self.movies = json[@"movies"];
-                               NSLog(@"response: %@", self.movies);
-                               NSLog(@"count: %lu", self.movies.count);
-        
-                               // reload data
-                               [self.tableView reloadData];
-    }];
-    */
     self.title = @"Movies";
 
 }
@@ -122,21 +101,11 @@
             self.movies = response.movies;
             [self.tableView reloadData];
         } failure:^(NSError *error) {
-            [[[UIAlertView alloc] initWithTitle:@"Error"
-                                        message:@"An error occurred"
-                                       delegate:nil
-                              cancelButtonTitle:@"OK"
-                              otherButtonTitles:nil, nil] show];
+            [self showConnectionError];
         }];
-        
     }
     @catch (NSException * e) {
-        NSLog(@"An Exception occurred: %@", e);
-        [[[UIAlertView alloc] initWithTitle:@"Error"
-                                    message:@"An error occurred"
-                                   delegate:nil
-                          cancelButtonTitle:@"OK"
-                          otherButtonTitles:nil, nil] show];
+        [self showConnectionError];
     }
     @finally {
         NSLog(@"finally");
@@ -145,6 +114,14 @@
     
 }
 
+-(void)showConnectionError {
+    [[[UIAlertView alloc] initWithTitle:@"Error"
+                                message:@"Couldn't get Movies. Check your connection"
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil, nil] show];
+
+}
 - (void)onRefresh {
     [self.restController getBoxOffice:@"10" country:@"us" apikey:kApiKey success:^(RTBoxOfficeResult *response) {
         NSLog(@"Response = %@", response);
@@ -152,11 +129,7 @@
         [self.refreshControl endRefreshing];
         [self.tableView reloadData];
     } failure:^(NSError *error) {
-        [[[UIAlertView alloc] initWithTitle:@"Error"
-                                    message:@"An error occurred"
-                                   delegate:nil
-                          cancelButtonTitle:@"OK"
-                          otherButtonTitles:nil, nil] show];
+        [self showConnectionError];
         [self.refreshControl endRefreshing];
     }];
 
